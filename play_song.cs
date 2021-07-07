dc5b21163f7466e70e84cebc7ca96eb26906b138
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2021-07-07 23:25:10 +0530

    Play song with vlc

---
+++ play_song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Play_Single_Music
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strCmdText;
+            // Replace FILE_PATH and filename
+            strCmdText = "/C start vlc FILE_PATH\\filename.mp3";
+
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+    }
+}
